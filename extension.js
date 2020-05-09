35719e3a6bbe85dea9a245622509b6297660d711
actboy168 <actboy168@gmail.com>
2020-05-09 11:00:18 +0800

    简化代码

--- extension.js
+++ extension.js
@@ -1,28 +1,20 @@
 const vscode = require('vscode');
-const path = require("path");
+const path = require('path');
 const os = require('os');
 const fs = require('fs');
 
 function dataFolderName() {
-    let product = JSON.parse(fs.readFileSync(path.join(vscode.env.appRoot, 'product.json')));
+    const product = JSON.parse(fs.readFileSync(path.join(vscode.env.appRoot, 'product.json')));
     if (vscode.env.remoteName === undefined) {
         return product.dataFolderName;
     }
     return product.serverDataFolderName;
 }
 
-function extensionPath() {
-    return path.join(os.homedir(), dataFolderName() + '/extensions/')
-}
-
 function activate(context) {
     context.subscriptions.push(vscode.commands.registerCommand("extensionPath", () => {
-        return extensionPath()
+        return path.join(os.homedir(), dataFolderName(), 'extensions', path.sep);
     }));
 }
 
-function deactivate() {
-}
-
 exports.activate = activate;
-exports.deactivate = deactivate;

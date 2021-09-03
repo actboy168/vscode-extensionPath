fea9be69344442859af8d79b6581284ff6844542
actboy168 <actboy168@gmail.com>
2021-09-03 09:58:31 +0800

    似乎没必要写得这么复杂

--- extension.js
+++ extension.js
@@ -1,19 +1,10 @@
 const vscode = require('vscode');
 const path = require('path');
-const os = require('os');
-const fs = require('fs');
-
-function dataFolderName() {
-    const product = JSON.parse(fs.readFileSync(path.join(vscode.env.appRoot, 'product.json')));
-    if (vscode.env.remoteName === undefined) {
-        return product.dataFolderName;
-    }
-    return product.serverDataFolderName;
-}
 
 function activate(context) {
+    const extensionPath = path.dirname(context.extensionPath);
     context.subscriptions.push(vscode.commands.registerCommand("extensionPath", () => {
-        return path.join(os.homedir(), dataFolderName(), 'extensions', path.sep);
+        return extensionPath;
     }));
 }
 
